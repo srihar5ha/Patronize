3aa51cb9dc44e2167f222f7233a433d894cd914b
srihar5ha <sri3har5ha@gmail.com>
2024-01-07 01:09:07 +0530

    landing linked

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from "../styles/Landing.module.css";
+import Link from "next/link";
+import Button from '@mui/material/Button';
+
 
 const LandingPage = () => {
   return (
     <div className={styles.landingPage}>
-       <button className="explore">Explore</button>
+            <Button variant="contained" color="primary" size="large" href="/content">
+              <Link href="/content" className="explore">Explore</Link>
+
+            </Button>
+
   </div>
   );
 };
